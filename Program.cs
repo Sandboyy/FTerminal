694175b37784fe53b2089e29ac823f047c97b751
Ale Dionisio <alessandrodionisio6@gmail.com>
2026-07-07 22:32:32 -0300

    Iniciado estudo de conceito

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+string[] docs = Directory.GetFiles(@"D:\repositoriosC#\FTerminal\Uses", "*.txt").Select(f => Path.GetFileName(f)).ToArray();
+
+foreach (string arq in docs)
+{
+    Console.WriteLine(Path.GetFileNameWithoutExtension(arq));
+}
